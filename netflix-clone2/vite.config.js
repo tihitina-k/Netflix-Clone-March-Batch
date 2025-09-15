b3070b4c6c91c1f8a91e813b9aaced24119b7893
tihitina-k <tihitinak19@gmail.com>
2025-09-15 06:13:46 +0300

    to deploy

--- netflix-clone2/vite.config.js
+++ netflix-clone2/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/netflix-clone2/'  // <-- Important for GitHub Pages
+  base: '/netflix-clone-march-batch/'  // <-- Important for GitHub Pages
 });
